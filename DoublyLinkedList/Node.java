1bf7fcdf44b8d8b4e6120e6f04c9d31642898fd3
Varun Nair <varun199700@gmail.com>
2020-05-11 20:32:41 +0530

    Create Node.java

---
+++ DoublyLinkedList/Node.java
@@ -0,0 +1,13 @@
+package DoublyLinkedList;
+
+public class Node {
+	int data;
+	Node nextElement;
+	Node prevElement;
+	public Node(int data) {
+		super();
+		this.data = data;
+		nextElement = null;
+		prevElement = null;
+	}
+}
